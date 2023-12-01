f80c42e69435c65e78a1ef9299e5df72e2ff2e50
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2023-12-01 10:34:18 +0200

    master: oops

--- lib/day_1/task.rb
+++ lib/day_1/task.rb
@@ -11,6 +11,7 @@ module Day1
       'eight' => 8,
       'nine' => 9
     }.freeze
+
     def initialize(sample)
       @sample = sample
     end
@@ -21,18 +22,6 @@ module Day1
         "#{res.first}#{res.last}".to_i
       end.sum
     end
-
-    WORDS_TO_NUMBERS = {
-      'one' => 1,
-      'two' => 2,
-      'three' => 3,
-      'four' => 4,
-      'five' => 5,
-      'six' => 6,
-      'seven' => 7,
-      'eight' => 8,
-      'nine' => 9
-    }.freeze
 
     def call2
       input.sum do |line|
