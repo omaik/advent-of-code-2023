9e25fe6ea4e09132c2f530c2003359c172c9981c
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2023-12-03 08:33:42 +0200

    master: day 3

--- lib/day_3/task.rb
+++ lib/day_3/task.rb
@@ -4,9 +4,49 @@ module Day3
       @sample = sample
     end
 
-    def call1; end
+    def call1
+      numbers = collect_numbers
 
-    def call2; end
+      numbers.map(&:first).sum
+    end
+
+    def call2
+      numbers = collect_numbers
+
+      numbers.select do |x|
+        x[1] == '*'
+      end.group_by { |x| x[3] }.values.select { |x| x.size == 2 }.map { |y| y.map(&:first).inject(:*) }.sum
+    end
+
+    def collect_numbers
+      input.each.with_index.with_object([]) do |(line, y), numbers|
+        last_x = 0
+        while match_data = line[last_x..]&.match(/\d+/)
+
+          start = match_data.begin(0) + last_x
+          ending = match_data.end(0) + last_x
+
+          (start...ending).map do |x|
+            adjacent(x, y)
+          end.flatten(1).each do |adj_x, adj_y|
+            next unless input[adj_y] && input[adj_y][adj_x]
+
+            next unless input[adj_y][adj_x] != '.' && !input[adj_y][adj_x].match(/\d/)
+
+            numbers << [match_data[0].to_i, input[adj_y][adj_x], [start..ending, y], [adj_x, adj_y]]
+            break
+          end
+
+          last_x = ending + 1
+        end
+      end
+    end
+
+    def adjacent(x, y)
+      [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+       [x - 1, y], [x + 1, y],
+       [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]]
+    end
 
     def input
       @input ||= Input.call(@sample)
--- spec/lib/day_3/task_spec.rb
+++ spec/lib/day_3/task_spec.rb
@@ -6,7 +6,7 @@ describe Day3::Task do
       let(:sample) { true }
 
       it 'works' do
-        expect(task.call1).to eq(nil)
+        expect(task.call1).to eq(4361)
       end
     end
 
@@ -14,7 +14,7 @@ describe Day3::Task do
       let(:sample) { false }
 
       it 'works' do
-        expect(task.call1).to eq(nil)
+        expect(task.call1).to eq(536_202)
       end
     end
   end
@@ -24,7 +24,7 @@ describe Day3::Task do
       let(:sample) { true }
 
       it 'works' do
-        expect(task.call2).to eq(nil)
+        expect(task.call2).to eq(467_835)
       end
     end
 
@@ -32,7 +32,7 @@ describe Day3::Task do
       let(:sample) { false }
 
       it 'works' do
-        expect(task.call2).to eq(nil)
+        expect(task.call2).to eq(78_272_573)
       end
     end
   end
