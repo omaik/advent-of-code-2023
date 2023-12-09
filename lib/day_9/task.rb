4be0008632beb62a15c0b144f06efb40fa0a9897
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2023-12-09 10:02:29 +0200

    master: day 9

--- lib/day_9/task.rb
+++ lib/day_9/task.rb
@@ -4,9 +4,33 @@ module Day9
       @sample = sample
     end
 
-    def call1; end
+    def call1
+      input.map do |line|
+        conses = build_tree(line)
+        conses.map(&:last).sum
+      end.sum
+    end
 
-    def call2; end
+    def call2
+      input.map do |line|
+        conses = build_tree(line)
+        conses.reverse.inject(0) do |acc, cons|
+          cons.first - acc
+        end
+      end.sum
+    end
+
+    def build_tree(line)
+      [line].tap do |conses|
+        loop do
+          conses << conses.last.each_cons(2).map do |a, b|
+            b - a
+          end
+
+          break if conses.last.all?(&:zero?)
+        end
+      end
+    end
 
     def input
       @input ||= Input.call(@sample)
--- spec/lib/day_9/task_spec.rb
+++ spec/lib/day_9/task_spec.rb
@@ -6,7 +6,7 @@ describe Day9::Task do
       let(:sample) { true }
 
       it 'works' do
-        expect(task.call1).to eq(nil)
+        expect(task.call1).to eq(114)
       end
     end
 
@@ -14,7 +14,7 @@ describe Day9::Task do
       let(:sample) { false }
 
       it 'works' do
-        expect(task.call1).to eq(nil)
+        expect(task.call1).to eq(1_974_913_025)
       end
     end
   end
@@ -24,7 +24,7 @@ describe Day9::Task do
       let(:sample) { true }
 
       it 'works' do
-        expect(task.call2).to eq(nil)
+        expect(task.call2).to eq(2)
       end
     end
 
@@ -32,7 +32,7 @@ describe Day9::Task do
       let(:sample) { false }
 
       it 'works' do
-        expect(task.call2).to eq(nil)
+        expect(task.call2).to eq(884)
       end
     end
   end
